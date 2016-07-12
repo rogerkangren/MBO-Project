0b3d2ecc8100cf9490f50ed6ba486f138b651e04
Ren <xsc4037@compassbnk.com>
2016-07-12 11:03:13 -0500

    Bill Anderson

--- Add-Officers-10-county.sql
+++ Add-Officers-10-county.sql
@@ -37,28 +37,28 @@ declare @newOfficer_ID int
 declare @newOfficer_StateCountyID int
 declare @NMLS_status nvarchar(10)
 
-set @newOfficer_lo_code = '14604' --int
-set @newOfficer_name = 'Chad Montgomery' --nvarchar(50)
-set @newOfficer_State = 'Alabama'--nvarchar(50)  --Alabama Arizona California Colorado Florida New Mexico Texas
-set @newOfficer_County = 'Madison' --nvarchar(50)
-set @newOfficer_County2 = 'DeKalb'--nvarchar(50)
-set @newOfficer_County3 = 'Jackson'--nvarchar(50)
-set @newOfficer_County4 = 'Marshall'--nvarchar(50)
-set @newOfficer_County5 = ''--nvarchar(50)
-set @newOfficer_County6 = ''--nvarchar(50)
-set @newOfficer_County7 = ''--nvarchar(50)
-set @newOfficer_County8 = ''--nvarchar(50)
-set @newOfficer_County9 = ''--nvarchar(50)
-set @newOfficer_County10 = '' --nvarchar(50)
-set @newOfficer_street_address = '114 Governors Dr. SE 2nd FL, Huntsville, AL 35801' --nvarchar(max)
-set @newOfficer_office_phone_number = '256-532-6273' --nvarchar(50)
-set @newOfficer_cell_phone_number = '' --nvarchar(50)
-set @newOfficer_fax_number = '205-524-0447' --nvarchar(50)
-set @newOfficer_site_effective_date = '4/25/2016' --datetime
-set @newOfficer_nmls_originator_id = '534943' --int
-set @newOfficer_email_address = 'chad.montgomery@bbva.com' --nvarchar(50)
-set @newOfficer_main_url = 'mortgage.bbvacompass.com/default.asp?siteId=7BA3E584-8A29-4218-9241-7B10307C430C' --nvarchar(max)
-set @newOfficer_alias_url = 'http://www.bbvacompass.com/mortgages/cmontgomery' --nvarchar(max)
+set @newOfficer_lo_code = '97835' --int
+set @newOfficer_name = 'Bill Anderson' --nvarchar(50)
+set @newOfficer_State = 'Texas'--nvarchar(50)  --Alabama Arizona California Colorado Florida New Mexico Texas
+set @newOfficer_County = 'Cameron' --nvarchar(50)
+set @newOfficer_County2 = 'Willacy'--nvarchar(50)
+set @newOfficer_County3 = 'Hidalgo'--nvarchar(50)
+set @newOfficer_County4 = 'Starr'--nvarchar(50)
+set @newOfficer_County5 = 'Kennedy'--nvarchar(50)
+set @newOfficer_County6 = 'Brooks'--nvarchar(50)
+set @newOfficer_County7 = 'Jim Hogg'--nvarchar(50)
+set @newOfficer_County8 = 'Zapata'--nvarchar(50)
+set @newOfficer_County9 = 'Jim Wells'--nvarchar(50)
+set @newOfficer_County10 = 'Duval' --nvarchar(50)
+set @newOfficer_street_address = '115 E Van Buren Harlingen, TX 78550' --nvarchar(max)
+set @newOfficer_office_phone_number = '956-429-8996' --nvarchar(50)
+set @newOfficer_cell_phone_number = '956-429-8996' --nvarchar(50)
+set @newOfficer_fax_number = '205-524-0677' --nvarchar(50)
+set @newOfficer_site_effective_date = '07/12/2016' --datetime
+set @newOfficer_nmls_originator_id = '1509108' --int
+set @newOfficer_email_address = 'bill.andersonjr@bbva.com ' --nvarchar(50)
+set @newOfficer_main_url = 'mortgage.bbvacompass.com/default.asp?siteId=E84E0FA3-199D-4914-8AA0-59C1BB7A5094' --nvarchar(max)
+set @newOfficer_alias_url = 'http://www.bbvacompass.com/mortgages/banderson' --nvarchar(max)
 set @newOfficer_profile_pic_path = 'https://mortgage.bbvacompass.com/Skins/images/carousel2.jpg' --nvarchar(50)
 set @newOfficer_business_hours = '' --nvarchar(50)
 set @newOfficer_site_status = 'Live' --nvarchar(50)
